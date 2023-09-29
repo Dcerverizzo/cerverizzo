43942e0652dfec50a1a02a69efe4619af3b5fec0
Daniel Cerverizzo <daniel.cerverizzo@cobrefacil.com.br>
2023-09-29 15:20:08 -0300

    fix: fix links languages / frameworks

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -91,7 +91,7 @@ c-18 -3 -37 0 -49 10 -19 13 -20 29 -25 228 -5 196 -7 216 -27 255 -13 23 -38
 
         {/* SVG Icons */}
 
-        <a href="https://react.dev" target="_blank" className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100 no-underline" rel="noreferrer">
+        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100 no-underline" rel="noreferrer">
           <svg width="14" height="12" className="text-js-primary mr-1" viewBox="0 0 128 128">
             <path fill="#F0DB4F" d="M1.408 1.408h125.184v125.185H1.408z"></path>
             <path fill="#323330" d="M116.347 96.736c-.917-5.711-4.641-10.508-15.672-14.981-3.832-1.761-8.104-3.022-9.377-5.926-.452-1.69-.512-2.642-.226-3.665.821-3.32 4.784-4.355 7.925-3.403 2.023.678 3.938 2.237 5.093 4.724 5.402-3.498 5.391-3.475 9.163-5.879-1.381-2.141-2.118-3.129-3.022-4.045-3.249-3.629-7.676-5.498-14.756-5.355l-3.688.477c-3.534.893-6.902 2.748-8.877 5.235-5.926 6.724-4.236 18.492 2.975 23.335 7.104 5.332 17.54 6.545 18.873 11.531 1.297 6.104-4.486 8.08-10.234 7.378-4.236-.881-6.592-3.034-9.139-6.949-4.688 2.713-4.688 2.713-9.508 5.485 1.143 2.499 2.344 3.63 4.26 5.795 9.068 9.198 31.76 8.746 35.83-5.176.165-.478 1.261-3.666.38-8.581zM69.462 58.943H57.753l-.048 30.272c0 6.438.333 12.34-.714 14.149-1.713 3.558-6.152 3.117-8.175 2.427-2.059-1.012-3.106-2.451-4.319-4.485-.333-.584-.583-1.036-.667-1.071l-9.52 5.83c1.583 3.249 3.915 6.069 6.902 7.901 4.462 2.678 10.459 3.499 16.731 2.059 4.082-1.189 7.604-3.652 9.448-7.401 2.666-4.915 2.094-10.864 2.07-17.444.06-10.735.001-21.468.001-32.237z"></path>
@@ -99,7 +99,7 @@ c-18 -3 -37 0 -49 10 -19 13 -20 29 -25 228 -5 196 -7 216 -27 255 -13 23 -38
           Javascript
         </a>
 
-        <a href="https://react.dev" target="_blank" className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100 no-underline" rel="noreferrer">
+        <a href="https://www.w3.org/Style/CSS/Overview.en.html" target="_blank" className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100 no-underline" rel="noreferrer">
           <svg width="14" height="12" className="text-css-primary mr-1" viewBox="0 0 128 128">
             <path fill="#1572B6" d="M18.814 114.123L8.76 1.352h110.48l-10.064 112.754-45.243 12.543-45.119-12.526z"></path>
             <path fill="#33A9DC" d="M64.001 117.062l36.559-10.136 8.601-96.354h-45.16v106.49z"></path>
@@ -127,7 +127,7 @@ c-18 -3 -37 0 -49 10 -19 13 -20 29 -25 228 -5 196 -7 216 -27 255 -13 23 -38
           React
         </a>
 
-        <a href="https://react.dev" target="_blank" className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100 no-underline" rel="noreferrer">
+        <a href="https://www.typescriptlang.org/" target="_blank" className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100 no-underline" rel="noreferrer">
           <svg width="14" height="12" className="text-typescript-primary mr-1" viewBox="0 0 128 128">
             <path fill="#fff" d="M22.67 47h99.67v73.67H22.67z"></path>
             <path data-name="original" fill="#007acc" d="M1.5 63.91v62.5h125v-125H1.5zm100.73-5a15.56 15.56 0 017.82 4.5 20.58 20.58 0 013 4c0 .16-5.4 3.81-8.69 5.85-.12.08-.6-.44-1.13-1.23a7.09 7.09 0 00-5.87-3.53c-3.79-.26-6.23 1.73-6.21 5a4.58 4.58 0 00.54 2.34c.83 1.73 2.38 2.76 7.24 4.86 8.95 3.85 12.78 6.39 15.16 10 2.66 4 3.25 10.46 1.45 15.24-2 5.2-6.9 8.73-13.83 9.9a38.32 38.32 0 01-9.52-.10 23 23 0 01-12.72-6.63c-1.15-1.27-3.39-4.58-3.25-4.82a9.34 9.34 0 011.15-.73L82 101l3.59-2.08.75 1.11a16.78 16.78 0 004.74 4.54c4 2.1 9.46 1.81 12.16-.62a5.43 5.43 0 00.69-6.92c-1-1.39-3-2.56-8.59-5-6.45-2.78-9.23-4.5-11.77-7.24a16.48 16.48 0 01-3.43-6.25 25 25 0 01-.22-8c1.33-6.23 6-10.58 12.82-11.87a31.66 31.66 0 019.49.26zm-29.34 5.24v5.12H56.66v46.23H45.15V69.26H28.88v-5a49.19 49.19 0 01.12-5.17C29.08 59 39 59 51 59h21.83z"></path>
@@ -135,7 +135,7 @@ c-18 -3 -37 0 -49 10 -19 13 -20 29 -25 228 -5 196 -7 216 -27 255 -13 23 -38
           Typescript
         </a>
 
-        <a href="https://react.dev" target="_blank" className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100 no-underline" rel="noreferrer">
+        <a href="https://rubyonrails.org/" target="_blank" className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100 no-underline" rel="noreferrer">
           <svg width="14" height="12" className="text-ruby-primary mr-1" viewBox="0 0 128 128">
             <linearGradient id="ruby-original-a" gradientUnits="userSpaceOnUse" x1="157.08" y1="2382.05" x2="131.682" y2="2426.892" gradientTransform="matrix(1 0 0 -1 -47.5 2517)">
               <stop offset="0" stopColor="#FB7655"></stop>
