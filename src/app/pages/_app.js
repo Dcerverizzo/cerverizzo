36826f01af121d36055843b1db361881e3b5d7f0
Daniel Cerverizzo <daniel.cerverizzo@cobrefacil.com.br>
2023-09-30 23:54:19 -0300

    feat: add google tag manager

---
+++ src/app/pages/_app.js
@@ -0,0 +1,33 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+const GA_TRACKING_ID = 'G-WZSTD0P2R9'; 
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag('config', GA_TRACKING_ID, {
+        page_path: url,
+      });
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', GA_TRACKING_ID);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
