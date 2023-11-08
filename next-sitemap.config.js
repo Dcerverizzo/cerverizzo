f45a09695fbe01fff49bf7c04ac5753c92b3a166
Daniel Cerverizzo <daniel.cerverizzo@cobrefacil.com.br>
2023-11-08 11:14:31 -0300

    feat: implements next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,44 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: 'https://cerverizzo.dev/',
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+  generateRobotsTxt: true,
+  // alternateRefs: [
+  //   {
+  //     href: 'https://es.example.com',
+  //     hreflang: 'es',
+  //   },
+  //   {
+  //     href: 'https://fr.example.com',
+  //     hreflang: 'fr',
+  //   },
+  // ],
+  // Default transformation function
+  transform: async (config, path) => {
+    return {
+      loc: 'blog', // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    }
+  },
+  additionalPaths: async (config) => [
+    await config.transform(config, '/blog'),
+  ],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: 'test-bot',
+        allow: ['/blog'],
+      },
+    ],
+  },
+}
