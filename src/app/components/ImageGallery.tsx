26c7e7c73cb9851b821b75c2d9e33a8c1eb33867
Daniel Cerverizzo <daniel.cerverizzo@cobrefacil.com.br>
2024-03-01 20:45:44 -0300

    fix: adjustment in images gallery

--- src/app/components/ImageGallery.tsx
+++ src/app/components/ImageGallery.tsx
@@ -1,77 +1,73 @@
 import Image from 'next/image';
+import { ReactElement, JSXElementConstructor, ReactNode, PromiseLikeOfReactNode, JSX } from 'react';
 
 export default function ImageGallery() {
+
   const images = [
-    '/images/speaking_1.jpeg',
-    '/images/sistemasbr_speaking.jpg',
-    '/images/myself.JPG',
-    '/images/didactica_team.jpg',
-    '/images/meet.jpg',
-    '/images/graduation.jpeg',
+    {
+      name: "speaking_1",
+      src: '/images/speaking_1.jpeg',
+      alt: "Man speaking on stage at Rotary conference",
+      classDiv: "relative h-40 mb-4",
+      classImage: "rounded-lg"
+    },
+    {
+      name: "sistemasbr_speaking",
+      src: '/images/sistemasbr_speaking.jpg',
+      alt: "Me standing with the SistemasBR team",
+      classDiv: "relative h-80 mb-4 sm:mb-4",
+      classImage: "relative rounded-lg object-[-16px] sm:object-center"
+    },
+    {
+      name: "myself",
+      src: '/images/myself.JPG',
+      alt: "Me speaking about Front End World",
+      classDiv: "relative h-80 mb-4",
+      classImage: "rounded-lg object-top sm:object-center"
+    },
+    {
+      name: "didactica_team",
+      src: '/images/didactica_team.jpg',
+      alt: "Me and the Didactica tech team",
+      classDiv: "relative h-40 mb-4 sm:mb-4",
+      classImage: "rounded-lg object-[-16px] sm:object-center"
+    },
+    {
+      name: "meet",
+      src: '/images/meet.jpg',
+      alt: "Me and Cobre Facil team celebrating new goals",
+      classDiv: "relative h-40 mb-4",
+      classImage: "rounded-lg"
+    },
+    {
+      name: "graduation",
+      src: '/images/graduation.jpeg',
+      alt: "Me standing on graduation day in college",
+      classDiv: "relative h-80",
+      classImage: "rounded-lg"
+    },
   ];
 
+  const renderedImages: any = [];
+
+  images.forEach((item) => {
+    renderedImages.push(
+      <div className={item.classDiv} key={item.name}>
+        <Image
+          loading="eager"
+          alt={item.alt}
+          src={item.src}
+          layout="fill"
+          objectFit="cover"
+          className={item.classImage}
+        />
+      </div>
+    )
+  });
+
   return (
-    <div className="columns-2 sm:columns-3 gap-4 my-8">
-      <div className="relative h-40 mb-4">
-        <Image
-          loading="eager"
-          alt="Me speaking on stage at Rotary conference"
-          src="/images/speaking_1.jpeg"
-          layout="fill"
-          objectFit="cover"
-          className="rounded-lg"
-        />
-      </div>
-      <div className="relative h-80 mb-4 sm:mb-0">
-        <Image
-          loading="eager"
-          alt="Me and the SistemasBR team"
-          src="/images/sistemasbr_speaking.jpg"
-          layout="fill"
-          objectFit="cover"
-          className="rounded-lg object-[-16px] sm:object-center"
-        />
-      </div>
-      <div className="relative h-40 sm:h-80 sm:mb-4">
-        <Image
-          loading="eager"
-          alt="Me speaking about Front End World"
-          src="/images/myself.JPG"
-          layout="fill"
-          objectFit="cover"
-          className="rounded-lg object-top sm:object-center"
-        />
-      </div>
-      <div className="relative h-40 mb-4 sm:mb-0">
-        <Image
-          loading="eager"
-          alt="Me and the Didactica tech team"
-          src="/images/didactica_team.jpg"
-          layout="fill"
-          objectFit="cover"
-          className="rounded-lg"
-        />
-      </div>
-      <div className="relative h-40 mb-4">
-        <Image
-          loading="eager"
-          alt="Me and Cobre Facil team celebrating"
-          src="/images/meet.jpg"
-          layout="fill"
-          objectFit="cover"
-          className="rounded-lg"
-        />
-      </div>
-      <div className="relative h-80">
-        <Image
-          loading="eager"
-          alt="Me standing on graduation day in college"
-          src="/images/graduation.jpeg"
-          layout="fill"
-          objectFit="cover"
-          className="rounded-lg"
-        />
-      </div>
+    <div className="columns-3 gap-4 my-8">
+      {renderedImages}
     </div>
   );
 }
